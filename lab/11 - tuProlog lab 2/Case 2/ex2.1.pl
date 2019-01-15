78ec1c02546cd883978abcc3c0dc81277e503411
Niccolò Maltoni <nicco.mlt@gmail.com>
2019-01-16 00:32:04 +0100

    Exercise 2.1 of lab 11 done

---
+++ lab/11 - tuProlog lab 2/Case 2/ex2.1.pl
@@ -0,0 +1,11 @@
+% Ex2.1: fromList
+% fromList(+List, -Graph)
+
+fromList([_], []).
+fromList([H1,H2|T], [e(H1,H2)|L]) :- fromList([H2|T], L).
+
+% • Just analyse the code
+% • It obtains a graph from a list
+%   – fromList([10,20,30],[e(10,20),e(20,30)]).
+%   – fromList([10,20],[e(10,20)]).
+%   – fromList([10],[]).
