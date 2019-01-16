7b6f78e49867845594640a43132be0e4c6cbf289
Niccolò Maltoni <nicco.mlt@gmail.com>
2019-01-16 15:20:59 +0100

    Exercise 2.4 of lab 11 done

---
+++ lab/11 - tuProlog lab 2/Case 2/ex2.4.pl
@@ -0,0 +1,21 @@
+% Ex2.4: reaching
+
+% reaching(+Graph, +Node, -List)
+% all the nodes that can be reached in 1 step from Node
+% possibly use findall, looking for e(Node,_) combined
+% with member(?Elem,?List)
+
+% findall/3 : findall(?term, +callable_term, ?list)
+% findall(Template, Goal, List)
+%
+% member/2 : member(?term, +list)
+% member(Element, List)
+
+% • Implement it!
+% • reaching([e(1,2),e(1,3),e(2,3)],1,L). -> L/[2,3]
+% • reaching([e(1,2),e(1,2),e(2,3)],1,L). -> L/[2,2]).
+
+reaching(G, N, L) :-
+  findall(X, member(e(N, X), G), L1),
+  findall(Y, member(e(Y, N), G), L2),
+  append(L1, L2, L).
